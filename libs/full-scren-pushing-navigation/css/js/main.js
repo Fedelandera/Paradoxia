76cd3135ee77ab9e7a9bc9d4e950000723830abf
Fedelandera <fede.landera@gmail.com>
2025-04-28 16:54:54 +0000

    actualizado main.js con la funcionalidad real de la pagina

--- libs/full-scren-pushing-navigation/css/js/main.js
+++ libs/full-scren-pushing-navigation/css/js/main.js
@@ -1,18 +1,18 @@
-(function() {
-    var navTrigger = document.querySelector('.cd-nav-trigger');
-    var nav = document.getElementById('cd-nav');
-  
-    if (navTrigger && nav) {
-      navTrigger.addEventListener('click', function(event) {
-        event.preventDefault();
-        nav.classList.toggle('open');
-      });
-  
-      var links = document.querySelectorAll('.cd-primary-nav li a');
-      links.forEach(function(link) {
-        link.addEventListener('click', function() {
-          nav.classList.remove('open');
-        });
-      });
-    }
-  })();
+jQuery(document).ready(function($){
+    // Abrir menú
+    $('.cd-nav-trigger').on('click', function(){
+        $('#cd-nav').addClass('is-visible');
+    });
+
+    // Cerrar menú al hacer click en los links
+    $('.cd-primary-nav a').on('click', function(){
+        $('#cd-nav').removeClass('is-visible');
+    });
+
+    // Cerrar menú al hacer click en el fondo
+    $('.cd-navigation-wrapper').on('click', function(e){
+        if( $(e.target).is('.cd-navigation-wrapper') ) {
+            $('#cd-nav').removeClass('is-visible');
+        }
+    });
+});
