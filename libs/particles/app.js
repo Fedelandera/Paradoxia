198096115deb58b3bc8c6403e7646e6ef00cc88f
Fedelandera <fede.landera@gmail.com>
2025-04-30 22:22:13 +0000

    agregados a html particles

---
+++ libs/particles/app.js
@@ -0,0 +1,31 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 80 },
+        "color": { "value": "#ffffff" },
+        "shape": {
+            "type": "circle",
+            "stroke": { "width": 0, "color": "#000000" }
+        },
+        "opacity": { "value": 0.5 },
+        "size": { "value": 3 },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "out_mode": "out"
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": { "enable": true, "mode": "repulse" }
+        }
+    },
+    "retina_detect": true
+});
